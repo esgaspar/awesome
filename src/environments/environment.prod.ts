a4705eb796ad00efbd2f24057f4361d11ec0f9bf
Erick S. Gaspar <esgaspar@msn.com>
2018-06-19 21:14:57 -0300

    heroku server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+    // Initialize Firebase
+    firebaseConfig: {
+      apiKey: "AIzaSyC2nfaV2T64SjvBntVW_-LkZaxQkkIOFOA",
+      authDomain: "awesome-7893d.firebaseapp.com",
+      databaseURL: "https://awesome-7893d.firebaseio.com",
+      projectId: "awesome-7893d",
+      storageBucket: "awesome-7893d.appspot.com",
+      messagingSenderId: "532033546349"
+    }
 };
