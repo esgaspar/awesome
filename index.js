54d3f1971c3117269e854303b1d4d1e8ec7c2fcc
Erick S. Gaspar <esgaspar@msn.com>
2018-06-27 10:06:38 -0300

    'heroku'

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const app = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'src')))
-  .use(express.static(path.join(__dirname, 'node_modules')))
-  .get('/*', (req, res) => res.render('src/index'))
-  .listen(process.env.PORT || 8080);
+// app.engine('html', require('ejs').renderFile);
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+ 
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+ 
+// app.use('/', routes);
+// app.use('/users', users);
 
-  console.log('__dirname'+__dirname);
+// app()
+//   .use(express.static(path.join(__dirname, 'src')))
+//   .use(express.static(path.join(__dirname, 'node_modules')))
+//   .get('/*', (req, res) => res.render('src/index'))
+//   .listen(process.env.PORT || 8080);
+
+//   console.log('__dirname'+__dirname);
